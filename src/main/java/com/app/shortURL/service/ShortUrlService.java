155099be3a618fe804d95e87a084a4ae8e0dab07
abhigit19 <52894351+abhigit19@users.noreply.github.com>
2021-05-17 14:42:30 +0100

    Add files via upload

---
+++ src/main/java/com/app/shortURL/service/ShortUrlService.java
@@ -0,0 +1,36 @@
+package com.app.shortURL.service;
+
+import org.springframework.stereotype.Service;
+
+import java.net.MalformedURLException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.IntStream;
+
+@Service
+public class ShortUrlService {
+
+    public static final String HOST_URL = "http://localhost:8080/s/";
+    public static final String BASE62 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    private static Map<String, String> tinyUrlMap = new HashMap<>();
+
+    public String getShortUrl(String fullUrl) throws MalformedURLException {
+        String tinyName=getTinyNames();
+        String shortUrl = HOST_URL.concat(tinyName);
+        tinyUrlMap.put(tinyName, fullUrl);
+        return shortUrl;
+    }
+
+    private String getTinyNames() {
+        StringBuilder random = new StringBuilder();
+        IntStream.range(0, 5).forEach(
+                y -> random.append(BASE62.charAt((int) Math.floor(Math.random() * BASE62.length())))
+        );
+        return random.toString();
+    }
+    public Map<String, String> getTinyUrlMap(){
+        return ShortUrlService.tinyUrlMap;
+    }
+
+}
